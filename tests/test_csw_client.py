452af05f0b5a935462dad990be6df120369e7a24
streino <streino@disroot.org>
2023-09-12 16:21:38 +0200

    Parameterized tests

--- tests/test_csw_client.py
+++ tests/test_csw_client.py
@@ -7,13 +7,16 @@ from udata_csw.csw_client import CswClient
 from factories import CswRecordFactory
 from util import to_xml
 
+MAX_RECORDS = 5
+
 @pytest.fixture
 def client():
     return CswClient(faker.url(), skip_caps=True)
 
-@pytest.fixture
-def data(n=2):
-    return CswRecordFactory.create_batch(n)
+@pytest.fixture(params=range(0,MAX_RECORDS))
+def data(request):
+    return CswRecordFactory.create_batch(request.param)
+
 
 def test_get_ids(client, data, rmock):
     rmock.post(client.url, status_code=200, text=to_xml(data))
@@ -21,5 +24,14 @@ def test_get_ids(client, data, rmock):
     ids = list(client.get_ids())
 
     history = rmock.request_history
+    assert len(ids) == len(data)
+    assert ids == [d.id for d in data]
+
+@pytest.mark.parametrize("page_size", range(1,MAX_RECORDS+1))
+def test_get_ids_pagination(client, data, rmock, page_size):
+    rmock.post(client.url, status_code=200, text=to_xml(data))
+
+    ids = list(client.get_ids(page_size=page_size))
+
     assert len(ids) == len(data)
     assert ids == [d.id for d in data]
