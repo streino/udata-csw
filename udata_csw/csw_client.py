e1645d42d503139300ad599dd4d822c2b8b213cc
streino <streino@disroot.org>
2023-09-13 11:21:51 +0200

    Better logs

--- udata_csw/csw_client.py
+++ udata_csw/csw_client.py
@@ -29,28 +29,33 @@ class CswClient(object):
         return self._url
 
     def get_ids(self, page_size=10, limit=None):
+        log.debug(f"get_ids: page_size={page_size}, limit={limit}")
         limit = limit or sys.maxsize
         fetched = 0
 
         while True:
             maxrecords = min(page_size, limit - fetched)
 
-            log.debug(f"Fetching {maxrecords} ids from position {fetched}")
+            log.debug(f"fetching {maxrecords} ids from position {fetched}...")
+            # Output schema 'csw' is lighter and enough to get ids
             self._csw.getrecords2(startposition=fetched, maxrecords=maxrecords, esn='brief', outputschema=ns('csw'))
 
             if self._csw.results['returned'] == 0:
+                log.debug("stop: no results")
                 break
-
-            limit = min(limit, self._csw.results['matches'])
 
             ids = list(self._csw.records.keys())
             for i in ids:
                 yield i
 
             fetched += len(ids)
+            limit = min(limit, self._csw.results['matches'])
+            log.debug(f"fetched {len(ids)} additional records, {fetched} so far")
             if fetched >= limit:
+                log.debug("stop: reached limit")
                 break
 
     def get_record(self, id):
+        # Output schema must be 'gmd' to get the full record
         self._csw.getrecordbyid(id=[id], esn='full', outputschema=ns('gmd'))
         return self._csw.records[id]
